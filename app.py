0767ff83e643473cbf271bff1db092f15488faa1
azzouzi19 <96174084+azzouzi-source@users.noreply.github.com>
2023-04-18 13:16:47 +0000

    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
+
+OPTIONS = [
+  
+  {
+    'id': 1,
+    'title': 'TC',
+    'nubre_elev': 220,
+    'exist': True
+  },
+  {
+    'id': 2,
+    'title': '1Bac',
+    'nubre_elev': 210,
+    
+  },
+  {
+    'id': 3,
+    'title': '2Bac',
+    'nubre_elev': 160,
+    'exist': True
+  }
+]
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', options=OPTIONS, name_lycee="IbnKhaldoune")
+
+@app.route('api/option')
+def option():
+  return jsonify(OPTIONS)
 
 if __name__ == "__main__":
   app.run("0.0.0.0", debug=True)
+
+
